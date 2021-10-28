51575e9da36d2597a3d5f5032936398d5790d9de
Polina <polina.staneva@yahoo.com>
2021-10-28 12:04:03 +0300

    try2

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  var innerHTML =  document.querySelectorAll(".price.hot").innerHTML;
+  var innerHTML =  document.querySelectorAll(".price.hot").textContent;
   // var hottey = document.querySelectorAll(".price.hot"); 
-  var flame =  "🔥";
-innerHTML.appendChild(flame);
-});
+  var flame = String.fromCodePoint(0x1f525);
+innerHTML = innerHTML + flame; 
+}
+
+)
